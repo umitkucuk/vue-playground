850de03da5c8600c4822c30607869536f969a33f
umitkucuk <umitkucuk@users.noreply.github.com>
2018-07-06 19:05:03 +0300

    Make improvements on index and about pages

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h2>Hey! I found your IP address: <el-tag>{{ ip }}</el-tag> 😎</h2>
+  </section>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    asyncData({ req, params }) {
+      return axios.get('https://api.ipify.org?format=json')
+        .then((res) => {
+          return { ip: res.data.ip }
+        })
+    }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,31 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt-starter
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+  <section>
+    <h2>Hello World!</h2>
+    <p class="lead">Let's have a look at source code of this project 💪</p>
+    <br>
+    <a href="https://github.com/umitkucuk/vue-playground" target="_blank">
+      <el-button type="primary">Source Code</el-button>
+    </a>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
-export default {
-  components: {
-    AppLogo
+  export default {
+  
   }
-}
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="scss">
+  section {
+    padding: 20px 0;
+  }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  h2 {
+    margin-bottom: 10px;
+  }
+  
+  p.lead {
+    font-size: 18px;
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  }
 </style>
