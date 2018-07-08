a2142df74e1cab3d65ab12cf59a524c00a7875ec
umitkucuk <umitkucuk@users.noreply.github.com>
2018-07-08 15:45:20 +0300

    Complete fetching all data

---
+++ components/Loading/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <div class="loading">
+      <span>Loading...</span>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  .loading {
+    padding: 20px 0;
+    text-align: center;
+    
+    span {
+      font-size: 18px;
+    }
+  }
+</style>
--- pages/sprints.vue
+++ pages/sprints.vue
@@ -9,38 +9,23 @@
         width="40">
       </el-table-column>
       <el-table-column
-        prop="fields.Epic Name"
-        label="Epic Name"
-        width="180">
+        prop="fields.Name"
+        label="Name"
+        width="200">
       </el-table-column>
       <el-table-column
-        prop="fields.Blocks Release"
-        label="Blocks Release"
+        prop="fields.Target Release Date"
+        label="Target Release Date"
         width="170">
       </el-table-column>
       <el-table-column
-        prop="fields.Epic Title"
-        label="Epic Title"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="fields.Description"
-        label="Description"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="fields.RobEst"
-        label="RobEst"
-        width="70">
-      </el-table-column>
-      <el-table-column
-        prop="fields.Blocked by (Epic)"
-        label="Blocked by (Epic)"
-        width="140">
-      </el-table-column>
-      <el-table-column
         prop="fields.Stories"
-        label="Stories">
+        label="Stories"
+        width="350">
+      </el-table-column>
+      <el-table-column
+        prop="fields.Notes"
+        label="Notes">
       </el-table-column>
     </el-table>
   </section>
@@ -56,7 +41,7 @@
   @Component({
     components: { Loading }
   })
-  export default class Epics extends Vue {
+  export default class Sprints extends Vue {
     items = []
     isLoading = true
     
@@ -66,7 +51,7 @@
     }
 
     loadItems () {
-      axios.get('https://api.airtable.com/v0/appNQ5GNluxZKls51/Epics?api_key=keymJwFHjpkwy7E3C')
+      axios.get('https://api.airtable.com/v0/appNQ5GNluxZKls51/Sprints?api_key=keymJwFHjpkwy7E3C')
         .then(response => this.items = response.data.records)
         .catch(error => console.log(error))
     }
