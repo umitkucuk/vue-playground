f036d6a7d4e32c0eac2c2b7a8f4e38b5646c4335
2ez4salt <talhaslt@gmail.com>
2018-11-13 02:17:15 +0300

    late changes

--- pages/SaltPage.vue
+++ pages/SaltPage.vue
@@ -1,40 +1,60 @@
 <template>
+  <div style="text-align: center; align-items: center; align-content: center">
   <section>
-
-
-
+    <br>
     <h2>Salt Vue Examples</h2>
     <br>
     <hr>
     <br>
-    <el-row>
-      <el-col :span="4">
         <div>
-          <el-input placeholder="Enter Your Name Please" v-model="input"></el-input>
+          <el-input placeholder="Please Enter Your Name" v-model="input"></el-input>
         </div>
-      </el-col>
-    </el-row>
-    <br>
+        <br>
+        <el-button :plain="true" type="primary" round @click="greeting(input) ">
+          Click Me
+        </el-button>
 
-    <el-button :plain="true" type="primary" round @click="greeting(input) ">
-      Click Me
-    </el-button>
+        <div>
+          <el-table
+            :data="arr"
+            style="width: auto"
+          >
+            <el-table-column
+              prop=""
+              label="Inputs"
+            >
+            </el-table-column>
+          </el-table>
+
+        </div>
+
 
   </section>
+  </div>
 </template>
 
 <script>
-
   import Vue from 'vue'
   import Component from 'nuxt-class-component'
 
 
   @Component
   export default class SaltPage extends Vue {
-
-    greeting (input) {
-        this.$message( "Welcome " + input + value12)
+    data() {
+      return {
+        value2: 3
+      }
     }
+    arr = [];
+    greeting (input) {
+        this.$message( "Welcome " + input);
+        console.log(this.arr + input);
+        this.arr.push(input);
+        console.log(this.arr)
+
+    }
+
+
   }
 </script>
 
