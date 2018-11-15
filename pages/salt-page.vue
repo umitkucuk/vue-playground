30d62a96913873322c2a3689c02cc141f7e813b4
2ez4salt <talhaslt@gmail.com>
2018-11-15 18:44:21 +0300

    Failed disabling button

--- pages/salt-page.vue
+++ pages/salt-page.vue
@@ -1,44 +1,49 @@
 <template>
   <div style="text-align: center; align-items: center; align-content: center">
-  <section>
-    <br>
-       <h2>Salt Vue Examples</h2>
-    <br>
-    <hr>
-    <br>
+    <section>
+      <br>
+      <h2>Salt Vue Examples</h2>
+      <br>
+      <hr>
+      <br>
       <h3>
         Greeting
       </h3>
-    <br>
-        <div>
-          <el-input placeholder="Please Enter Your Name" v-model="input"></el-input>
-        </div>
-        <br>
-        <el-button :plain="true" type="primary" round @click="greeting(input) ">
+      <br>
+      <div>
+        <el-input v-bind:id="inputx" type="text" placeholder="Please Enter Your Name" v-model="inputx"></el-input>
+      </div>
+      <br>
+      <keep-alive>
+        <el-button :plain="true" type="primary" v-bind:disabled=!isDisable  v-on:click="greeting(inputx)" >
           Click Me
         </el-button>
-    <br><br>
-    <h3>Todo list comin.</h3>
-    <br>
-    <hr>
-    <br>
+      </keep-alive>
+      <el-button :plain="true" type="primary"   v-on:click="toggle()" >
+        Toggle
+      </el-button>
+      <br><br>
+      <h3>Todo list comin.</h3>
+      <br>
+      <hr>
+      <br>
 
-        <div>
-          <el-table
-            :data="arr"
-            style="width: auto"
+      <div>
+        <el-table
+          :data="arr"
+          style="width: auto"
+        >
+          <el-table-column
+            prop="arr"
+            label="Inputs"
           >
-            <el-table-column
-              prop="arr"
-              label="Inputs"
-            >
-            </el-table-column>
-          </el-table>
+          </el-table-column>
+        </el-table>
 
-        </div>
+      </div>
 
 
-  </section>
+    </section>
   </div>
 </template>
 
@@ -50,16 +55,33 @@
   @Component
   export default class SaltPage extends Vue {
     arr = [];
+    inputx = "";
+    isDisable = false;
 
-    greeting (input) {
-        this.$message( "Welcome " + input);
-        console.log(this.arr + input);
-        this.arr.push(input);
-        console.log(this.arr);
 
+    computed(){
+      this.toggle()
     }
 
 
+    toggle(){
+
+      if(this.inputx.length > 0){
+        this.isDisable=true
+      }else {
+        this.isDisable=false
+      }
+      console.log(this.isDisable)
+    }
+
+
+    greeting (inputx) {
+      this.$message( "Welcome " + this.inputx);
+      console.log(this.inputx.length);
+
+      this.arr.push(this.inputx);
+      console.log(this.arr);
+    }
   }
 </script>
 
